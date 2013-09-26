4d606f984ba6a41b3c68556a1476bc81479eac70
sergiokv13 <sergiokv13@gmail.com>
2013-09-25 21:27:34 -0400

    probando

---
+++ src/main/java/fizzbuzz/FizzBuzz.java
@@ -0,0 +1,32 @@
+package fizzbuzz;
+
+public class FizzBuzz {
+
+	private static final int _FIZZ = 3;
+	private static final int _BUZZ = 5;
+	private static final String _MENSAJE_BUZZ = "Buzz";
+	private static final String _MENSAJE_FIZZ = "Fizz";
+	private static final String _MENSAJE_FIZZ_BUZZ = "FizzBuzz";
+	
+	public String generar(int numero) {
+		if (esMultiploDe(numero, _FIZZ)&&esMultiploDe(numero, _BUZZ))
+			return _MENSAJE_FIZZ_BUZZ;
+		if (esMultiploDe(numero, _FIZZ))
+			return _MENSAJE_FIZZ;
+		if (esMultiploDe(numero, _BUZZ))
+			return _MENSAJE_BUZZ;
+		return Integer.toString(numero);	
+	}
+	
+	private boolean esMultiploDe(int numero, int multiplo) {
+		return numero % multiplo == 0;
+	}
+	
+	public String imprimir(int maximo) {
+		String resultado = "";	
+		for (int i = 1; i <= maximo; i++)
+			resultado = resultado + generar(i) + "\n";
+		return resultado;
+	}
+
+}
---
+++ src/test/java/fizzbuzz/FizzBuzzTest.java
@@ -0,0 +1,75 @@
+package fizzbuzz;
+
+import static org.junit.Assert.*;
+
+import org.junit.Before;
+import org.junit.Test;
+
+	public class FizzBuzzTest {
+	private FizzBuzz fb;
+	
+	@Before
+	public void setup() {
+		fb = new FizzBuzz();
+	}
+	
+	@Test
+	public void generaFizzSiNumeroEs3() {
+		assertEquals("Fizz",fb.generar(3));
+	}
+	
+	@Test
+	public void generaElMismoNumeroSiNoEs3()
+	{
+		assertEquals("4",fb.generar(4));
+	}
+	
+	@Test
+	public void generaFizzSiEsMultiploDe3()
+	{
+		assertEquals("Fizz",fb.generar(6));
+	}
+	
+	@Test
+	public void generaBuzzSiNumero5(){
+		assertEquals("Buzz",fb.generar(5));
+	}
+	
+	@Test
+	public void generaBuzzSiEsMultiplo5(){
+		assertEquals("Buzz",fb.generar(10));
+	}
+	
+	@Test
+	public void generaFizzBuzzPara15(){
+		assertEquals("FizzBuzz",fb.generar(15));
+	}
+	
+	@Test
+	public void generaFizzBuzzParaMultiploDe3y5(){
+		assertEquals("FizzBuzz",fb.generar(30));
+		assertEquals("FizzBuzz", fb.generar(60));
+	}
+	
+	@Test
+	public void generaElNumeroSiNoEsMultiploDe3Ni5(){
+		assertEquals("7", fb.generar(7));
+		assertEquals("8", fb.generar(8));
+	}
+	
+	@Test
+	public void imprimeUnaSecuenciaDeNumerosConReglasFizzBuzz(){
+		String esperado = "1\n"+
+				"2\n"+
+				"Fizz\n"+
+				"4\n"+
+				"Buzz\n" +
+				"Fizz\n";
+		assertEquals(esperado, fb.imprimir(6));
+	
+	}
+	
+	
+	
+
+}
